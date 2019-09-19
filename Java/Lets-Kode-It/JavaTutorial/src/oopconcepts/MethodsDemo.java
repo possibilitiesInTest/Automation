b62249d3d430bde577e31979f61487231b70da00
Gourav B <pit.wire@pm.me>
2019-09-19 10:27:27 -0400

    understanding return types

---
+++ Java/Lets-Kode-It/JavaTutorial/src/oopconcepts/MethodsDemo.java
@@ -0,0 +1,13 @@
+package oopconcepts;
+
+public class MethodsDemo {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "Hey there !!!";
+		System.out.println(a);
+		
+		
+	}
+}

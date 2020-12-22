7f2f3a7326dd4d8f7a202fd1a6ed4fe9a40863df
pit-dev <possibilitiesInTest@protonmail.com>
2020-12-21 20:31:49 -0500

    end-to-end form entry and validation

---
+++ Python-Selenium-Blair/projects/browser_automation/test_1.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+browser.get("https://techstepacademy.com/trial-of-the-stones")
+
+# Riddle of the Stones
+stone_input = browser.find_element_by_css_selector("input[id='r1Input']")
+stone_answer_butn = browser.find_element_by_css_selector("button#r1Btn")
+stone_result = browser.find_element_by_css_selector("div#passwordBanner > h4")
+
+secrets_input = browser.find.element_by_css_selector("input[id='r2Input']")
+secrets_answer_butn = browser.find.element_by_css_selector("button#r2Butn")
+
+# Two Merchants
+richest_merchant_name = browser.find_element_by_xpath("//[text()='3000']").text
+merchant_input = browser.find_element_by_id('r3input')
+merchant_answer_butn = browser.find_element_by_css_selector("button#r3Butn")
+check_butn = browser.find_element_by_css_selector("button[name='checkButn']")
+complete_msg = browser.find_element_by_css_selector("dic#trialCompleteBanner h4")
+
+
+stone_input.send_keys('rock')
+stone_answer_butn.click()
+password = stone_result.text
+
+secrets_input.send_keys(richest_merchant_name)
+secrets_answer_butn.click()
+
+check_butn.click()
+assert complete_msg.text == 'Trial Complete'

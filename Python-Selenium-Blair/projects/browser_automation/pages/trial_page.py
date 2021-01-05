fc72ca862b607abbf9fe085ace9b85068060b42a
pit-dev <possibilitiesInTest@protonmail.com>
2021-01-04 22:16:03 -0500

    refactor w. better locators

--- Python-Selenium-Blair/projects/browser_automation/pages/trial_page.py
+++ Python-Selenium-Blair/projects/browser_automation/pages/trial_page.py
@@ -2,27 +2,27 @@ from selenium.webdriver.common.by import By
 
 from base_element import BaseElement
 from base_page import BasePage
-
+from locator import Locator
 
 class TrialPage(BasePage):
     url = 'https://techstepacademy.com/trial-of-stones'
 
     @property
     def stone_input(self):
-        locator = (By.CSS_SELECTOR, 'input#r1Input')
-        return BaseElement(self.driver, by=locator[0], value=locator[1])
+        locator = Locator(By.CSS_SELECTOR, 'input#r1Input')
+        return BaseElement(self.driver, locator=locator)
 
     @property
     def stone_button(self):
-        locator = (By.ID, 'r1Btn')
-        return BaseElement(self.driver, by=locator[0], value=locator[1])
+        locator = Locator(By.ID, 'r1Btn')
+        return BaseElement(self.driver, locator=locator)
 
     @property
     def secrets_input(self):
-        locator = (By.CSS_SELECTOR, 'input#r2input')
-        return BaseElement(self.driver, by=locator[0], value=locator[1])
+        locator = Locator(By.CSS_SELECTOR, 'input#r2input')
+        return BaseElement(self.driver, locator=locator)
 
     @property
     def secrets_button(self):
-        locator = (By.CSS_SELECTOR, 'button#r2Butn')
-        return BaseElement(self.driver, by=locator[0], value=locator[1])
+        locator = Locator(By.CSS_SELECTOR, 'button#r2Butn')
+        return BaseElement(self.driver, locator=locator)

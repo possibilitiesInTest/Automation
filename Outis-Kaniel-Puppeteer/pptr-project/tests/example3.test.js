b4156be830e49a09992cbb10d83dbbe28262c9c5
pit-dev <possibilitiesInTest@protonmail.com>
2021-03-10 11:21:09 -0500

    chai assertions and default timeout:      expect(title).to.be.a('string', 'value')      expect(url).to.include('example.com')      expect(count)to.be.equal(2)'

--- Outis-Kaniel-Puppeteer/pptr-project/tests/example3.test.js
+++ Outis-Kaniel-Puppeteer/pptr-project/tests/example3.test.js
@@ -1,13 +1,17 @@
+const { expect } = require("chai");
 const puppeteer = require("puppeteer");
 
 describe("My third puppeteer test", () => {
   it("testing the browser", async function () {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 10,
       devtools: false,
     });
     const page = await browser.newPage();
+    await page.setDefaultTimeout(10000);
+    await page.setDefaultNavigationTimeout(20000);
+
     await page.goto("https://example.com/");
     const title = await page.title();
     const url = await page.url();
@@ -15,6 +19,11 @@ describe("My third puppeteer test", () => {
     const count = await page.$$eval("p", (elm) => elm.length);
     console.log("Text in the H1:" + text);
     console.log("Number of p tags: " + count);
+
+    expect(title).to.be.a("string", "Example Domain");
+    expect(url).to.include("example.com");
+    expect(text).to.be.a("string", "Example Domain");
+    expect(count).to.be.equal(2);
     await browser.close();
   });
 });

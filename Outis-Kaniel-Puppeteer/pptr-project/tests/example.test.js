84b482bb744d8298abef6c952f948343d21777bf
pit-dev <possibilitiesInTest@protonmail.com>
2021-03-10 10:14:14 -0500

    waitForTimeout(3000)

--- Outis-Kaniel-Puppeteer/pptr-project/tests/example.test.js
+++ Outis-Kaniel-Puppeteer/pptr-project/tests/example.test.js
@@ -2,9 +2,17 @@ const puppeteer = require("puppeteer");
 
 describe("my first puppeteer test", () => {
   it("should launch the browser", async function () {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 50,
+      devtools: false,
+    });
     const page = await browser.newPage();
     await page.goto("http://example.com/");
+    await page.waitForTimeout(3000);
+    await page.waitForSelector("h1");
+    await page.reload();
+    await page.waitForTimeout(3000);
     await page.waitForSelector("h1");
     await browser.close();
   });

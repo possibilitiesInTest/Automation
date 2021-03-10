a5c82ca40c87e1f9c7cb185d98f9bdd4259a645a
pit-dev <possibilitiesInTest@protonmail.com>
2021-03-10 15:37:15 -0500

    context = await browser.createIncognito... page = await context.newPage()

--- Outis-Kaniel-Puppeteer/pptr-project/tests/device.test.js
+++ Outis-Kaniel-Puppeteer/pptr-project/tests/device.test.js
@@ -10,7 +10,9 @@ describe("Device Emulation", () => {
       slowMo: 10,
       devtools: false,
     });
-    page = await browser.newPage();
+    const context = await browser.createIncognitoBrowserContext();
+
+    page = await context.newPage();
     await page.setDefaultTimeout(10000);
     await page.setDefaultNavigationTimeout(20000);
   });

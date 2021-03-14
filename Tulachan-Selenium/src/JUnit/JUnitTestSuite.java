a5e491f0460340a17a5cffc5de84ee338a24ea49
pit-dev <possibilitiesInTest@protonmail.com>
2021-03-14 19:53:01 -0400

    JUnit test @before, @after, @test, @RunWith testrunner

---
+++ Tulachan-Selenium/src/JUnit/JUnitTestSuite.java
@@ -0,0 +1,15 @@
+package JUnit;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        JUnitTest.class,
+        JunitTestBeforeAfterClass.class
+})
+
+public class JUnitTestSuite {
+    
+}

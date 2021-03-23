93960d80bc126f26fd6ed251b3f371aa01757926
pit-dev <possibilitiesInTest@protonmail.com>
2021-03-22 23:17:22 -0400

    using (DataTable data) for data driven tests

--- Shetty-Rahul-Cucumber/Automation/src/test/java/features/Login.feature
+++ Shetty-Rahul-Cucumber/Automation/src/test/java/features/Login.feature
@@ -13,3 +13,11 @@ Given User is on NETBanking landing page
 When User login into application with "john" and "4321"
 Then Homepage is populated
 And Cards displayed are "false"
+
+Scenario: Home page default login
+Given User is on NETBanking landing page
+When User login into application with the following details
+| jenny | abcd | john@abcd.com | Australia | 3245324 |
+
+Then Homepage is populated
+And Cards displayed are "false"
--- Shetty-Rahul-Cucumber/Automation/src/test/java/stepDefinitions/stepDefinition.java
+++ Shetty-Rahul-Cucumber/Automation/src/test/java/stepDefinitions/stepDefinition.java
@@ -1,6 +1,10 @@
 package stepDefinitions;
 
+import java.util.List;
+
 import org.junit.runner.RunWith;
+
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -37,6 +41,16 @@ public class stepDefinition {
         System.out.println("Displays cards: " + strArg1);
     }
     
+    @When("^User login into application with the following details$")
+    public void user_login_into_application_with_the_following_details(DataTable data) throws Throwable {
+    		List<List<String>> obj = data.asLists();
+    	System.out.println(obj.get(0).get(0));
+    	System.out.println(obj.get(0).get(1));
+    	System.out.println(obj.get(0).get(2));
+    	System.out.println(obj.get(0).get(3));
+    	System.out.println(obj.get(0).get(4));
+	
+    }
 
     
 

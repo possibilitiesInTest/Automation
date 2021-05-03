f7cc0b791c264d61c9d6d6a7cc30f441e3aa0d50
pit-dev <possibilitiesInTest@protonmail.com>
2021-05-03 08:47:41 -0400

    login action w. chai assertion to verify link text

--- Cypress-KK/cypress/integration/examples/ea.spec.js
+++ Cypress-KK/cypress/integration/examples/ea.spec.js
@@ -3,7 +3,15 @@
 describe("Testing of EA App", () => {
   it("login application", () => {
     cy.visit("http://eaapp.somee.com/");
-    cy.contains("Login").click();
+
+    // cy.contains("Login").click();
+
+    // Login action w. chai verify link text
+    cy.get("#loginLink").then(($link) => {
+      const linkText = $link.text();
+      expect(linkText).is.eql("Login");
+    });
+
     cy.url().should("include", "/Account/Login");
     cy.get("#UserName").type("admin");
     cy.get("#Password").type("password");

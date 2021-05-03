b1cce7df77393fbd759257950c2726b6e2ac7aa3
pit-dev <possibilitiesInTest@protonmail.com>
2021-05-03 11:08:44 -0400

    using alias 'as()' to check value and invoke() in place of promise()

--- Cypress-KK/cypress/integration/examples/ea.spec.js
+++ Cypress-KK/cypress/integration/examples/ea.spec.js
@@ -7,10 +7,25 @@ describe("Testing of EA App", () => {
     // cy.contains("Login").click();
 
     // Login action w. chai verify link text
-    cy.get("#loginLink").then(($link) => {
-      const linkText = $link.text();
-      expect(linkText).is.eql("Login");
+    // cy.get("#loginLink").then(($link) => {
+    //   const linkText = $link.text();
+    //   expect(linkText).is.eql("Login");
+    // });
+
+    cy.get("#loginLink").invoke("text").as("linkText");
+    //   .then(($link) => {
+    //     return $link.text();
+    //   })
+    //   .as("linkText");
+
+    cy.contains("Login").click();
+
+    cy.get("@linkText").then(($x) => {
+      expect($x).is.eql("Login");
     });
+
+    // expect will not work outside of codeblock
+    // expect(linkText).is.eql('Login');
 
     cy.url().should("include", "/Account/Login");
     cy.get("#UserName").type("admin");
@@ -23,7 +38,7 @@ describe("Testing of EA App", () => {
     cy.get(".table").find("tr > td");
     cy.get(".table")
       .find("tr")
-      .contains("Prasanth")
+      .contains("Prashanth")
       .parent()
       .contains("Benefits")
       .click();

b952ac3315ace7c23db9d35a5bd9875a92593e30
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-24 13:36:45 -0500

    absolute vs relative xpath

--- Dhingra-Selenium/QATechHubProject/src/guru99project/Guru_99_Project.java
+++ Dhingra-Selenium/QATechHubProject/src/guru99project/Guru_99_Project.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Guru_99_Project {
 
 	ChromeDriver driver;
-	
 	String url = "https://demo.guru99.com/v4";
 	
 	
@@ -23,7 +22,6 @@ public class Guru_99_Project {
 		WebElement usernameElement = driver.findElement(By.name("uid"));
 		usernameElement.sendKeys(username);
 		driver.findElement(By.name("password")).sendKeys(password);
-		
 		driver.findElement(By.name("btnLogin")).click();
 		
 	}
@@ -31,6 +29,15 @@ public class Guru_99_Project {
 	public void addCustomer() {
 		driver.findElement(By.linkText("New Customer")).click();
 		driver.findElement(By.name("name")).sendKeys("Saurabh");
+		
+		// absolute xpath
+		// difficulty: hardcoded, website may change
+		// /html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[2]
+		// relative xpath: slower, but more stable
+		// //htmlTagname[@attribute='value']
+		// // any child of html // 1 of 12 inputs
+		// @value equals some radio value f or m
+		
 		driver.findElement(By.xpath("//input[@value='f']")).click();
 		driver.findElement(By.name("dob")).sendKeys("06//21/1999");
 		driver.findElement(By.name("addr")).sendKeys("94234 Gurgaon");
@@ -40,9 +47,9 @@ public class Guru_99_Project {
 		driver.findElement(By.name("telephoneno")).sendKeys("435435435");
 		String emailId = "Test" + System.currentTimeMillis() + "@abc.com";
 		driver.findElement(By.name("emailid")).sendKeys(emailId);
-		
 		driver.findElement(By.name("password")).sendKeys("Pro@123");
 		driver.findElement(By.name("sub")).click();
 	}
+	
 	
 }

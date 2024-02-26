357ab5660993fad4134ecd9b94157780a7ae91c9
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-26 09:54:41 -0500

    implicit wait test to access elements after page load

--- Dhingra-Selenium/QATechHubProject/src/Timeouts/FlipkartProject.java
+++ Dhingra-Selenium/QATechHubProject/src/Timeouts/FlipkartProject.java
@@ -2,7 +2,10 @@ package Timeouts;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class FlipkartProject {
 
@@ -15,12 +18,28 @@ public class FlipkartProject {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(url);
+	}
+	
+	public void mouseHover() {
+		
+		// by passes pop-up
+		driver.findElement(By.xpath("//span[@class='_30XB9F']")).click();
+		
+		WebElement electronicsLink = driver.findElement(By.xpath("//span[text()]='Electronics'"));
+		Actions action = new Actions(driver);
+		action.moveToElement(electronicsLink).build().perform();
+		
+		WebElement samsungLink = driver.findElement(By.xpath("//a[text()='Samsung][1]"));
+		action.moveToElement(samsungLink).click().build().perform();
+		// build method accumulates all operations written before it then performs it
 	}
 	
 	public static void main(String[] args) {
 		
 		FlipkartProject fp = new FlipkartProject();
 		fp.invokeBrowser();
+		fp.mouseHover();
 	}
 }

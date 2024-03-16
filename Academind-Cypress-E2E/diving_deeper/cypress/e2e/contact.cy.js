76279d9b64467b60b3deb65567b3490bbc478cd6
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-16 00:59:17 -0400

    cypress: using blur() to unfocus elements, using should/then to avoid breaks in run mode

--- Academind-Cypress-E2E/diving_deeper/cypress/e2e/contact.cy.js
+++ Academind-Cypress-E2E/diving_deeper/cypress/e2e/contact.cy.js
@@ -30,4 +30,42 @@ describe('page navigation', () => {
         cy.get('@submitBtn').contains('Sending...');
         cy.get('@submitBtn').should('have.attr', 'disabled');  
     });
+
+    it('should validate the form input', () => {
+        cy.visit('http://localhost:5173/about');
+        cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+        cy.get('@submitBtn').click();
+        cy.get('@submitBtn').then(el => {
+            expect(el).to.not.have.attr('disabled');
+            expect(el.text()).to.not.equal('Sending...');
+        });
+        cy.get('@submitBtn').contains('Send Message');
+        cy.get('[data-cy="contact-input-message"]').blur();
+        cy.get('[data-cy="contact-input-message"]')
+            .parent()
+            .should('have.attr', 'class').and('match', /invalid/);
+
+
+            // fails in npx cypres run
+            // .then((el) => {
+            //     expect(el.attr('class')).to.contains('invalid');
+            // })
+        
+        cy.get('[data-cy="contact-input-name"]').focus().blur();
+        cy.get('[data-cy="contact-input-name"]')
+            .parent()
+            .should('have.attr', 'class').and('match', /invalid/);
+            // .then((el) => {
+            //     expect(el.attr('class')).to.contains('invalid');
+            // })
+
+        cy.get('[data-cy="contact-input-email"]').focus().blur();
+        cy.get('[data-cy="contact-input-email"]')
+            .parent()
+            .should('have.attr', 'class').and('match', /invalid/);
+            // .then((el) => {
+            //     expect(el.attr('class')).to.contains('invalid');
+            // })
+
+    });
 });

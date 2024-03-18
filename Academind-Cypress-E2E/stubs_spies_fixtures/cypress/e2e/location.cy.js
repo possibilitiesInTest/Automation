909d6a28343fa61fc170882d7eb6cac36cae9645
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-18 13:24:49 -0400

    outsourcing, storing, and providing fixture data to tests

--- Academind-Cypress-E2E/stubs_spies_fixtures/cypress/e2e/location.cy.js
+++ Academind-Cypress-E2E/stubs_spies_fixtures/cypress/e2e/location.cy.js
@@ -2,19 +2,17 @@
 
 describe('share location', () => {
   beforeEach(() => {
+    cy.fixture("user-location.json").as('userLocation');
     cy.visit('/').then((window) => {
-      cy.stub(window.navigator.geolocation, 'getCurrentPosition')
-      .as('getUserPosition')
-      .callsFake((cb) => {
-        setTimeout(() => {
-        cb({
-          coords: {
-            latitude: 37.5,
-            longitude: 48.01,
-          },
-        });
-      }, 100);
-      });
+      cy.get('@userLocation').then(fakePosition => {
+        cy.stub(window.navigator.geolocation, 'getCurrentPosition')
+        .as('getUserPosition')
+        .callsFake((cb) => {
+          setTimeout(() => {
+          cb(fakePosition);
+        }, 100);
+        });  
+      })
       cy.stub(window.navigator.clipboard, 'writeText')
       .as('saveToClipboard')
       .resolves();
@@ -40,9 +38,12 @@ describe('share location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="share-loc-btn').click();
     cy.get('@saveToClipboard').should('have.been.called');
-    cy.get('@saveToClipboard')
+    cy.get('@userLocation').then((fakePosition) => {
+      const {latitude, longitude} = fakePosition.coords;
+      cy.get('@saveToClipboard')
       .should('have.been.calledWithMatch', 
-      new RegExp(`${37.5}.*${48.01}.*${encodeURI('John Doe')}`)
+      new RegExp(`${latitude}.*${longitude}.*${encodeURI('John Doe')}`)
     )      
+    });
   });
 });

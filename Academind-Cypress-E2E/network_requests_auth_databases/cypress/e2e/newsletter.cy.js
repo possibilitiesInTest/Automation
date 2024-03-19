1bb3c5cb657aeb328faf8ef4779365f349a10dbb
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-19 17:00:02 -0400

    intercept() for validation errors in http requests

--- Academind-Cypress-E2E/network_requests_auth_databases/cypress/e2e/newsletter.cy.js
+++ Academind-Cypress-E2E/network_requests_auth_databases/cypress/e2e/newsletter.cy.js
@@ -13,4 +13,14 @@ describe('Newsletter', () => {
         cy.wait('@subscribe');
         cy.contains("Thanks for signing up!");
     });
+    it('validation errors displayed when attempting to sign up for newsletter', () => {
+        cy.intercept('POST', '/newsletter*', {message: 'Email exists already'}).as('subscribe');
+        cy.visit('/');
+        cy.get('[data-cy="newsletter-email"]').click()
+        cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+        cy.get('[data-cy="newsletter-submit"]').click();
+        cy.wait('@subscribe');
+        cy.contains("Email exists already");
+        
+    });
 });
